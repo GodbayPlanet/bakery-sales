5743731ac3a20040c3a06818ceb5fb4d428f7d28
Nemanja Vasic <nemanjavasa@gmail.com>
2017-10-30 20:06:16 +0100

    First commit

---
+++ src/main/java/com/sales/bakery/BakerySalesApplication.java
@@ -0,0 +1,12 @@
+package com.sales.bakery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakerySalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakerySalesApplication.class, args);
+	}
+}
