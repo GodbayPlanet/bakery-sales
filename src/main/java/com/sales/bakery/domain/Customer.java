8c3b37bca5d5ff1d76f3b21373d779dbc0112ead
Nemanja Vasic <nemanjavasa@gmail.com>
2017-10-30 20:12:18 +0100

    Added com.sales.bakery.domain package and Customer class

---
+++ src/main/java/com/sales/bakery/domain/Customer.java
@@ -0,0 +1,30 @@
+package com.sales.bakery.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Customer {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	private String firstName;
+	private String lastName;
+	
+	public Customer(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+}
